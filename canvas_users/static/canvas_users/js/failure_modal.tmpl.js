3ad364f0161c60449913cc51fbbff40c4733f1be
Jim Laney <jlaney@uw.edu>
2017-09-26 10:09:24 -0700

    adds better UI messaging for canvas non-200 responses

--- canvas_users/static/canvas_users/js/failure_modal.tmpl.js
+++ canvas_users/static/canvas_users/js/failure_modal.tmpl.js
@@ -6,7 +6,7 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
   var buffer = "", stack1, helper, functionType="function", escapeExpression=this.escapeExpression;
 
 
-  buffer += "<div class=\"content-box\">\n  <div class=\"grid-row delayed\">\n    <div class=\"col-sm-2 uw-add-user-problem-icon\"></div>\n    <div class=\"col-sm-10\">\n      <div>\n        <p class=\"bad-result\">\n          We're terribly sorry, but it appears we're unable to add users at this time.\n        </p>\n        <p>\n          Please click <span class=\"start-over\">Start Over to give it another try now, or </span>Close to try again later.\n          <div class=\"hidden\">\n            Reported Error: ";
+  buffer += "<div class=\"content-box\">\n  <div class=\"grid-row delayed\">\n    <div class=\"col-sm-2 uw-add-user-problem-icon\"></div>\n    <div class=\"col-sm-10\">\n      <div>\n        <p class=\"bad-result\">\n          We're terribly sorry, but we were unable to add these users to this course.\n        </p>\n        <p>\n          You can click <span class=\"start-over\">Start Over to give it another try now, or </span>Done to try again later.\n          <div class=\"error-msg\">\n            Reported Error: ";
   if (helper = helpers.error_message) { stack1 = helper.call(depth0, {hash:{},data:data}); }
   else { helper = (depth0 && depth0.error_message); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
   buffer += escapeExpression(stack1)
