a88d235eb0f98d889b609ca39563250d10903828
Jim Laney <jlaney@uw.edu>
2020-11-20 09:23:24 -0800

    updates django to 2.2.13

--- setup.py
+++ setup.py
@@ -20,10 +20,10 @@ setup(
     packages=['canvas_users'],
     include_package_data=True,
     install_requires = [
-        'Django>=2.1,<2.2',
+        'Django>=2.2.13,<3.0',
         'django-blti>=2.2.1',
-        'uw-memcached-clients>=1.0.2,<2.0',
-        'UW-RestClients-Canvas>=1.1.10,<2.0',
+        'uw-memcached-clients>=1.0.5,<2.0',
+        'UW-RestClients-Canvas>=1.1.12,<2.0',
     ],
     license='Apache License, Version 2.0',
     description=(
