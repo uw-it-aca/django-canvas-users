7eb2122816ac9590659165c4cb4ae665884a6faa
mike seibel <mikes@washington.edu>
2025-03-17 13:13:20 -0700

    remove middleware test

--- docker/settings.py
+++ docker/settings.py
@@ -11,11 +11,6 @@ INSTALLED_APPS += [
     'canvas_users.apps.CanvasUsersConfig',
 ]
 
-MIDDLEWARE += [
-    'blti.middleware.LTISessionAuthenticationMiddleware',
-    'blti.middleware.PartitionedCookieMiddleware',
-]
-
 if os.getenv('SIS_PROVISIONER_ENV') in RESTCLIENTS_DEFAULT_ENVS:
     RESTCLIENTS_SIS_PROVISIONER_DAO_CLASS = 'Live'
     RESTCLIENTS_SIS_PROVISIONER_TIMEOUT = RESTCLIENTS_DEFAULT_TIMEOUT
