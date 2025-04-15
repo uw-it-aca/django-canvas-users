65e02de34905aef1f43be9816638fe13432f09ff
mike seibel <mikes@washington.edu>
2025-04-15 07:45:16 -0700

    discard local cors

--- canvas_users/views/__init__.py
+++ canvas_users/views/__init__.py
@@ -10,39 +10,6 @@ from blti import BLTIException
 import re
 
 
-def allow_origin(origin):
-    canvas_host = settings.RESTCLIENTS_CANVAS_HOST
-    if origin != canvas_host:
-        m = re.match(r'^https://.*\.([a-z]+\.[a-z]+)$', origin)
-        if m:
-            domain = m.group(1)
-            if canvas_host[-len(domain):] == domain:
-                canvas_host = origin
-
-    return canvas_host
-
-
-def add_headers_for_view(view, **kwargs):
-    return
-    if hasattr(view, 'blti'):
-        canvas_host = 'https://{}'.format(view.blti.canvas_api_domain)
-    else:
-        http_origin = view.request.META.get('HTTP_ORIGIN', '')
-        canvas_host = allow_origin(http_origin)
-
-    response = kwargs.get('response')
-    response['Access-Control-Allow-Methods'] = ', '.join(
-        view._allowed_methods())
-    response['Access-Control-Allow-Headers'] = ', '.join([
-        'Content-Type', 'X-SessionId', 'X-CSRFToken', 'X-CSRF-Token',
-        'X-Requested-With'])
-    response['Access-Control-Allow-Origin'] = canvas_host
-
-    if view.request.method == 'OPTIONS':
-        response['Allow'] = ', '.join(view._allowed_methods())
-        response['Content-Length'] = '0'
-
-
 class LaunchView(BLTILaunchView):
     template_name = 'canvas_users/launch_add_user.html'
     authorized_role = 'admin'
@@ -76,9 +43,6 @@ class AddUsersView(BLTIView):
             'http_host': self.request.META['HTTP_HOST']
         }
 
-    def add_headers(self, **kwargs):
-        add_headers_for_view(self, **kwargs)
-
     def options(self, request, *args, **kwargs):
         return self.render_to_response({})
 
@@ -89,9 +53,6 @@ class UserRESTDispatch(RESTDispatch):
     @csrf_exempt
     def dispatch(self, request, *args, **kwargs):
         return super(UserRESTDispatch, self).dispatch(request, *args, **kwargs)
-
-    def options(self, request, *args, **kwargs):
-        return self.json_response(status=200)
 
     def add_headers(self, **kwargs):
         add_headers_for_view(self, **kwargs)
